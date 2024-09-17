454300607854d23457a7c4959db3177117ae229f
harshjaiswal01 <harshjaiswal@gmail.com>
2024-09-16 18:37:15 -0600

    vision board pic_url

--- models/schemas/vision_board_schema.py
+++ models/schemas/vision_board_schema.py
@@ -6,6 +6,7 @@ class VisionBoardSchema(Schema):
     is_custom = fields.Bool(dump_only=True)
     description = fields.Str(required=False)
     created_by = fields.Int(dump_only=True)
+    pic_url = fields.Str(required=False)
 
 vision_board_schema = VisionBoardSchema()
 vision_boards_schema = VisionBoardSchema(many=True)
--- models/vision_board.py
+++ models/vision_board.py
@@ -9,14 +9,16 @@ class VisionBoard(Base):
     name: Mapped[str] = mapped_column(db.String(100), nullable=False)
     is_custom: Mapped[bool] = mapped_column(db.Boolean, default=False)
     description: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    pic_url: Mapped[str] = mapped_column(db.String(500), nullable=False)
     created_by: Mapped[Optional[int]] = mapped_column(db.Integer, db.ForeignKey('users.id'), nullable=True)
 
     user: Mapped[Optional["User"]] = relationship("User", back_populates="vision_boards")  # Explicit relationship to User
     content_items: Mapped[List["ContentItem"]] = db.relationship("ContentItem", back_populates="vision_board", cascade="all, delete-orphan")
     user_vision_boards: Mapped[List["UserVisionBoard"]] = db.relationship("UserVisionBoard", back_populates="vision_board", cascade="all, delete-orphan")
 
-    def __init__(self, name, is_custom=False, created_by=None, description = None):
+    def __init__(self, name, is_custom=False, created_by=None, description = None, pic_url = None):
         self.name = name
         self.is_custom = is_custom
         self.created_by = created_by
         self.description = description
+        self.pic_url = pic_url
